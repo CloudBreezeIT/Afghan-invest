a0a3e804e633d233ece4281940eaca50a7716f08
Usman Ali <Usman.tpr@gmail.com>
2024-07-15 16:24:39 +0500

    added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Banner from "../Components/Home/Banner";
 import Service from "../Components/Home/Service";
 import NewsAndMedia from "../Components/Home/NewsAndMedia";
+import Founders from "../Components/Home/Founders";
+import Address from "../Components/Home/Address";
+import Footer from "../Components/Footer";
+import Gallery from "../Components/Gallery";
 
 const Home = () => {
   return (
@@ -10,6 +14,10 @@ const Home = () => {
       <About />
       <Service />
       <NewsAndMedia />
+      <Founders />
+      <Gallery />
+      <Address />
+      <Footer />
     </>
   );
 };
