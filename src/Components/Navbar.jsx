31463e38e628e3dafe0349b9c90f476b91cb5193
Usman Ali <Usman.tpr@gmail.com>
2024-07-15 17:03:06 +0500

    added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
            <li><Link to='/' className='text-[17px] font-semibold'>HOME</Link></li>    
-          <li><Link className='text-[17px] font-semibold'>ABOUT US</Link></li>
+          <li><Link to='/about' className='text-[17px] font-semibold'>ABOUT US</Link></li>
           <li><Link className='text-[17px] font-semibold'>PRODUCTS</Link></li>
           <li><Link className='text-[17px] font-semibold'>GALLERY</Link></li>
           <li><Link className='text-[17px] font-semibold'>CONTACT US</Link></li>
