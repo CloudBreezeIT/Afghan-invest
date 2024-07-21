30ed465878de4df31b018f3d37ba59f8f38090fe
Waqas Mehmood <waqmeh456@gmail.com>
2024-07-21 14:15:24 +0500

    home routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Navbar from './Components/Navbar'
-import AboutUs from './Pages/AboutUs'
-import Footer from './Components/Footer'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import AboutUs from './Pages/AboutUs';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
-    <BrowserRouter>
-     <Navbar /> 
+    <BrowserRouter basename="/Afghan-invest">
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<AboutUs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
       <Footer />
-
-
     </BrowserRouter>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
