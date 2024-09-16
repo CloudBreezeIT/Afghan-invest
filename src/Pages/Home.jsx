9eb6273590f875b1d16c90fe289e1d3c18f2cd1c
Muhammad Bilal <mbilal45422@gmail.com>
2024-09-16 15:14:26 +0500

    some changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,6 @@ import Service from "../Components/Home/Service";
 import NewsAndMedia from "../Components/Home/NewsAndMedia";
 import Founders from "../Components/Home/Founders";
 import Address from "../Components/Home/Address";
-import Footer from "../Components/Footer";
 import Gallery from "../Components/Home/Gallery"
 import About from "../Components/Home/About";
 
