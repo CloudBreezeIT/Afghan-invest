76fd8f74d00de4f534b84b63ff4b1f79f94697bf
Muhammad Bilal <mbilal45422@gmail.com>
2024-09-17 13:08:13 +0500

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -38,7 +38,16 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
             <li><Link to='/' className='text-[17px] font-semibold'>{t('header.first-menu')}</Link></li>  
-            <li><Link to='/about' className='text-[17px] font-semibold'>{t('header.second-menu')}</Link></li>
+            <details>
+              <summary  className='text-[17px] font-semibold cursor-pointer'>{t('header.second-menu')}</summary>
+              <ul className=''>
+              <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-1')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-2')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-3')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-4')}</Link></li>
+              </ul>
+            </details>
+            <li><Link to='' className='text-[17px] font-semibold'></Link></li>
             <li><Link to='/exhibition' className='text-[17px] font-semibold'>{t('header.third-menu')}</Link></li>
             <li><Link to='/events' className='text-[17px] font-semibold'>{t('header.fourth-menu')}</Link></li>
             <li><Link to='/articles' className='text-[17px] font-semibold'>{t('header.fifth-menu')}</Link></li>
@@ -51,8 +60,17 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 space-x-2">
-          <li><Link to='/' className='text-[17px] font-semibold'>{t('header.first-menu')}</Link></li>    
-          <li><Link to='/about'className='text-[17px] font-semibold'>{t('header.second-menu')}</Link></li>
+          <li><Link to='/' className='text-[17px] font-semibold'>{t('header.first-menu')}</Link></li>  
+          <details>
+              <summary className='text-[17px] font-semibold cursor-pointer'>{t('header.second-menu')}</summary>
+              <ul className=''>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-1')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-2')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-3')}</Link></li>
+                <li className='text-[15px] p-1 font-semibold'><Link>{t('header.second-sub-menu-4')}</Link></li>
+              </ul>
+            </details>  
+          <li><Link to=''className='text-[17px] font-semibold'></Link></li>
           <li><Link to='exhibitions' className='text-[17px] font-semibold'>{t('header.third-menu')}</Link></li>
           <li><Link to='/events' className='text-[17px] font-semibold'>{t('header.fourth-menu')}</Link></li>
           <li><Link to='/articles' className='text-[17px] font-semibold'>{t('header.fifth-menu')}</Link></li>
@@ -84,7 +102,7 @@ const Navbar = () => {
         <FiSearch className={`${showInput ? 'block' :"hidden"} cursor-pointer`} size={25} onClick={() => setShowInput(!showInput)}/>
         <input 
           type="text" 
-          placeholder='Search Here ...'
+          placeholder={t('input.search-field')}
           className={`transition-all duration-300 ease-in-out border-black border-2 px-3 rounded-3xl ${showInput ? 'w-0 opacity-0' : 'block w-44 opacity-100'} focus-within::border-red-700`} 
           style={{ height: '2.5rem' }}
         />
