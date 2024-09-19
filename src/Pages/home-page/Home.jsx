20ef0e3bc5148af4ecab974f47317cfaef6f53e6
Muhammad Bilal <mbilal45422@gmail.com>
2024-09-19 13:08:07 +0500

    museum info section completed

--- src/Pages/home-page/Home.jsx
+++ src/Pages/home-page/Home.jsx
@@ -7,13 +7,15 @@ import Address from "../../Components/home-page-components/Address";
 import Gallery from "../../Components/home-page-components/Gallery";
 import About from "../../Components/home-page-components/About";
 import Hero from "../../Components/home-page-components/Hero";
+import MuseumInfoBar from "../../Components/home-page-components/MuseumInfoBar";
 
 const Home = () => {
   return (
     <>
       {/* <Banner /> */}
       <Hero />
-      <About />
+      <MuseumInfoBar/>
+      {/* <About /> */}
       <Service />
       <NewsAndMedia />
       <Founders />
